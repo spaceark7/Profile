4680873a883d008cfcb62de7174e8a08670a37f4
SpaceArk7 <spacearkchief@gmail.com>
2020-09-21 22:25:48 +0700

    Added Hero Section

---
+++ src/Components/test.jsx
@@ -0,0 +1,13 @@
+import React from 'react'; 
+const Kancut = () => {
+    return ( <div className="">
+        <slider>
+            <slide><p>Slide 1</p></slide>
+            <slide><p>Slide 1</p></slide>
+            <slide><p>Slide 1</p></slide>
+            <slide><p>Slide 1</p></slide>
+        </slider>
+    </div>  );
+}
+ 
+export default Kancut;
