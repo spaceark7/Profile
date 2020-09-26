73f6e33f988e579a9e168c0d375437b5d38a6492
SpaceArk& <spacearkchief@gmail.com>
2020-09-26 11:52:49 +0700

    About section updated

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,23 +10,26 @@ const About = () => {
             <div className="about-title">
               <h4>My Journey</h4>
               <h2>
-                ROAD TO BE AN EXPERT <br />
-                OF WEB DEVELOPER & GRAPHIC DESIGN
+                THE ROAD TO BE TECH TYCOON IS AHEAD <br />
               </h2>
             </div>
 
             <div className="about-experiences">
-              <p className="text-head">experiences</p>
+              <p className="text-head">background</p>
               <p className="text-description">
-                I dive into this field since 2014, started from a hobby then i
-                got curious about this <span>Web Development</span> Technology.
-                I've been create and build more than 100+ website to sharpen my
-                skill. Other than that i also develop{" "}
-                <span>Desktop Apps & Android Apps.</span> My basic skill is{" "}
-                <span>Graphic Design.</span> My current job is a content editor,
-                Graphic design and digital marketing for a retail company.
+                I dive into this field since 2012, Obsessed from Operating
+                System Programming, the first thing i leant is <span>C++</span>{" "}
+                Programming Language to strengthen my foundation about basic
+                programming. Then i continue my step towards
+                <span> Web Development</span> Technology. I've been create and
+                build more than 50+ website to sharpen my skill. Other than that
+                i also develop <span>Desktop Apps & Android Apps.</span> My
+                basic skill is <span>Graphic Design.</span> My current job is a
+                content editor, Graphic design and digital marketing for a
+                retail company.
               </p>
             </div>
+
             <div className="about-skills">
               <p className="text-head">skills</p>
               <div className="skill-list">
@@ -69,7 +72,31 @@ const About = () => {
               </div>
             </div>
           </div>
-          <div className="right-text col-md-6"></div>
+          <div className="right-text col-md-6">
+            <div className="about-title">
+              <h4>My Vision</h4>
+              <h2>
+                FORESEE THE FUTURE
+                <br />
+                MAKE IT HAPPENS
+              </h2>
+            </div>
+            <div className="about-experiences">
+              <p className="text-head">Vision</p>
+              <p className="text-description">
+                I dive into this field since 2012, Obsessed from Operating
+                System Programming, the first thing i leant is <span>C++</span>{" "}
+                Programming Language to strengthen my foundation about basic
+                programming. Then i continue my step towards
+                <span> Web Development</span> Technology. I've been create and
+                build more than 50+ website to sharpen my skill. Other than that
+                i also develop <span>Desktop Apps & Android Apps.</span> My
+                basic skill is <span>Graphic Design.</span> My current job is a
+                content editor, Graphic design and digital marketing for a
+                retail company.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
